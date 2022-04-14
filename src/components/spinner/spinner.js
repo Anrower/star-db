c54a3a880e36e6cba0486d576da85d48369971c6
Denis Shneider <anrowerdd@gmail.com>
2022-04-14 19:06:10 +0300

    feat: add spinner to random-planet companent

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-ccotb5l0jv">
+      <div class="ldio-d4gn3nsqd0a">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner;
