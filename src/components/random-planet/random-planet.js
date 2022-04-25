495b14e9e85774de8b683dc546e83922d1e5750c
Denis Shneider <anrowerdd@gmail.com>
2022-04-25 23:07:19 +0300

    feat: add default props for randomPlanet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -7,6 +7,10 @@ import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+  static defaultProps = {
+    updateInterval: 10000
+  };
+
   swapiService = new SwapiService();
 
   state = {
@@ -15,8 +19,9 @@ export default class RandomPlanet extends Component {
   }
 
   componentDidMount() {
+    const { updateInterval } = this.props
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 10000);
+    this.interval = setInterval(this.updatePlanet, updateInterval);
   }
 
   componentWillUnmount() {
@@ -65,7 +70,10 @@ export default class RandomPlanet extends Component {
       </div>
     );
   };
+
 };
+
+
 
 const PlanetView = ({ planet }) => {
 
