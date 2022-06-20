1028980d8cecb1a2c9eb14f6e025038ce1609bf4
Denis Shneider <anrowerdd@gmail.com>
2022-06-20 15:42:30 +0300

    feat: start work with react-router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,12 +6,13 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 import PeoplePage from '../pages/people.page';
-import PlanetPage from '../pages/planet-page';
-import StarshipPage from '../pages/starship-page';
+import PlanetsPage from '../pages/planet-page';
+import StarshipsPage from '../pages/starship-page';
 import { SwapiServiceProvider } from '../swapi-service-context';
 
-
 import './app.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
 
@@ -36,13 +37,18 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetPage />
-            <StarshipPage />
-          </div>
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
+              <Routes>
+                <Route path='/people' element={<PeoplePage />} />
+                <Route path='/planets' element={<PlanetsPage />} />
+                <Route path='/starships' element={<StarshipsPage />} />
+              </Routes>
+
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
